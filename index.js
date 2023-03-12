6836e93a69a6d689bcedc0ec280ea00b38ba018d
irwan suwandi <52568251+nawochus@users.noreply.github.com>
2023-03-12 09:20:49 +0700

    update

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From irwan" }');
+    res.send('{ "response": "Hello From irwan \n" }');
 });
 
 app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works! \n" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
